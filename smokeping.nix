286d763cee61168030713d1233f0fdf297a42090
Kai Hendry <hendry@iki.fi>
2023-01-26 13:54:16 +0000

    fixes https://github.com/kaihendry/NixOS-smokeping/issues/5

--- smokeping.nix
+++ smokeping.nix
@@ -7,7 +7,7 @@
       (builtins.getFlake "github:nixos/nixpkgs/e779fb4a661168a24db731e2e4fcd1d26c96985e").legacyPackages.x86_64-linux.smokeping;
     enable = true;
     port = 8081;
-    host = "192.168.1.40";
+    host = "0.0.0.0";
     targetConfig = builtins.readFile ./Targets;
   };
 }
