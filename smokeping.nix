9c3d44f1684ae22c88d461af3af7a43fc5567f28
Kai Hendry <hendry@iki.fi>
2023-01-28 19:36:01 +0800

    use rpi package fixes https://github.com/kaihendry/NixOS-smokeping/issues/6

--- smokeping.nix
+++ smokeping.nix
@@ -4,10 +4,12 @@
 }: {
   services.smokeping = {
     package =
-      (builtins.getFlake "github:nixos/nixpkgs/e779fb4a661168a24db731e2e4fcd1d26c96985e").legacyPackages.x86_64-linux.smokeping;
+      (builtins.getFlake "github:nixos/nixpkgs/e779fb4a661168a24db731e2e4fcd1d26c96985e").legacyPackages.aarch64-linux.smokeping;
     enable = true;
     port = 8081;
     host = "0.0.0.0";
+    owner = "Kai Hendry";
+    ownerEmail = "hendry+smokeping@iki.fi";
     targetConfig = builtins.readFile ./Targets;
   };
 }
